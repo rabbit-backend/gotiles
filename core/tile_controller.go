8002e571c13d1cdaad8a9e98fbc33f50e5500aa0
rohit20001221 <rohit20001221@gmail.com>
2025-11-11 23:43:08 +0530

    code refactor

---
+++ core/tile_controller.go
@@ -0,0 +1,49 @@
+package core
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rabbit-backend/go-tiles/db"
+	engine "github.com/rabbit-backend/template"
+)
+
+func NewTileController(connections map[string]db.DBSource, e *engine.Engine) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		x := c.Param("x")
+		y := c.Param("y")
+		z := c.Param("z")
+		tileName := c.Param("tile")
+		source := c.Param("source")
+
+		db := connections[source]
+
+		params := map[string]any{
+			"_x": x,
+			"_y": y,
+			"_z": z,
+		}
+
+		for key, value := range c.QueryParams() {
+			params[key] = value[0]
+		}
+
+		tileQueryPath := path.Join("tiles", "db", source, fmt.Sprintf("%s.sql", tileName))
+		query, args, err := e.Execute(tileQueryPath, params)
+		if err != nil {
+			log.Println("[x] error:", err)
+			return c.Blob(http.StatusInternalServerError, "application/x-protobuf", []byte(""))
+		}
+
+		data, err := db.Execute(query, args...)
+		if err != nil {
+			log.Println("[x] error:", err)
+			return c.Blob(http.StatusInternalServerError, "application/x-protobuf", []byte(""))
+		}
+
+		return c.Blob(http.StatusOK, "application/x-protobuf", data)
+	}
+}
--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
 	"path"
 
 	"github.com/joho/godotenv"
@@ -26,41 +23,7 @@ func main() {
 	e.Use(middleware.CORS())
 
 	e.Static("/static", path.Join("tiles", "static"))
-
-	e.GET("/tiles/:source/:tile/:x/:y/:z", func(c echo.Context) error {
-		x := c.Param("x")
-		y := c.Param("y")
-		z := c.Param("z")
-		tileName := c.Param("tile")
-		source := c.Param("source")
-
-		db := connections[source]
-
-		params := map[string]any{
-			"_x": x,
-			"_y": y,
-			"_z": z,
-		}
-
-		for key, value := range c.QueryParams() {
-			params[key] = value[0]
-		}
-
-		tileQueryPath := path.Join("tiles", "db", source, fmt.Sprintf("%s.sql", tileName))
-		query, args, err := core.GetQuery(sqlEngine, tileQueryPath, params)
-		if err != nil {
-			log.Println("[x] error:", err)
-			return c.Blob(http.StatusInternalServerError, "application/x-protobuf", []byte(""))
-		}
-
-		data, err := db.Execute(query, args...)
-		if err != nil {
-			log.Println("[x] error:", err)
-			return c.Blob(http.StatusInternalServerError, "application/x-protobuf", []byte(""))
-		}
-
-		return c.Blob(http.StatusOK, "application/x-protobuf", data)
-	})
+	e.GET("/tiles/:source/:tile/:x/:y/:z", core.NewTileController(connections, sqlEngine))
 
 	e.Use(middleware.Gzip())
 
