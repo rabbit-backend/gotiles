61964505220cbb7161eac5b0e382711aefcfdade
rohit20001221 <rohit20001221@gmail.com>
2025-11-16 18:52:39 +0530

    memsql cert path env variable

--- db/sources.go
+++ db/sources.go
@@ -4,5 +4,5 @@ import engine "github.com/rabbit-backend/template"
 
 var DB_SOURCES = map[string](func(e *engine.Engine) DBSource){
 	"postgres": func(e *engine.Engine) DBSource { return &PGSource{engine: e} },
-	"memsql":   func(e *engine.Engine) DBSource { return &MySQLSource{engine: e} },
+	"memsql":   func(e *engine.Engine) DBSource { return &MemSQLSource{engine: e} },
 }
